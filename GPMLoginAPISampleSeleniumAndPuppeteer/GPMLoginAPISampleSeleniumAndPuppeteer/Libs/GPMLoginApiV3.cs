251e6d0434a6378aef6c194628814fb0570c96e1
Duy Bui <buiducduy111@gmail.com>
2023-12-21 01:03:53 +0700

    first commit

---
+++ GPMLoginAPISampleSeleniumAndPuppeteer/GPMLoginAPISampleSeleniumAndPuppeteer/Libs/GPMLoginApiV3.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPMLoginAPISampleSeleniumAndPuppeteer.Libs
+{
+    public class GPMLoginApiV3
+    {
+        private const string START_ENDPOINT = "/api/v3/profiles/start/{id}";
+
+        private string _apiUrl;
+
+        public GPMLoginApiV3(string apiUrl)
+        {
+            _apiUrl = apiUrl;
+        }
+
+        /// <summary>
+        /// Start profile by id
+        /// </summary>
+        /// <param name="profileId"></param>
+        /// <returns>Return JObject or null</returns>
+        public async Task<JObject> StartProfileAsync(string profileId)
+        {
+            string apiUrl = $"{_apiUrl}{START_ENDPOINT}".Replace("{id}", profileId);
+            string resp = await httpGetAsync(apiUrl);
+
+            if (resp == null)
+                return null;
+
+            try
+            {
+                return JObject.Parse(resp);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        #region Helpers
+        private async Task<string> httpGetAsync(string apiUrl)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                        return await response.Content.ReadAsStringAsync();
+
+                    throw new Exception("Unknow error");
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ GPMLoginAPISampleSeleniumAndPuppeteer/GPMLoginAPISampleSeleniumAndPuppeteer/Program.cs
@@ -0,0 +1,52 @@
+﻿using GPMLoginAPISampleSeleniumAndPuppeteer.Libs;
+using Newtonsoft.Json.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using PuppeteerSharp;
+
+namespace GPMLoginAPISampleSeleniumAndPuppeteer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GPMLoginApiV3 api = new GPMLoginApiV3("http://127.0.0.1:19995");
+
+            JObject startResultObj = api.StartProfileAsync("17169ef5-761a-4fc4-9fba-2b634424c8c9").Result;
+            FileInfo gpmDriverFileInfo = new FileInfo(Convert.ToString(startResultObj["data"]["driver_path"]));
+            string remoteAddress = Convert.ToString(startResultObj["data"]["remote_debugging_address"]);
+
+            // Selenium
+            ChromeDriverService seleWebDriver = ChromeDriverService.CreateDefaultService(gpmDriverFileInfo.DirectoryName, gpmDriverFileInfo.Name);
+            ChromeOptions options = new ChromeOptions();
+            options.DebuggerAddress = remoteAddress;
+
+            ChromeDriver driver = new ChromeDriver(seleWebDriver, options);
+            driver.Navigate().GoToUrl("https://giaiphapmmo.vn");
+
+            Console.ReadLine();
+
+            // Puppeter
+            string remoteBrowserEndpoint = "http://" + remoteAddress;
+            ConnectOptions pupLaunchOptions = new ConnectOptions()
+            {
+                BrowserURL = remoteBrowserEndpoint
+            };
+            var browser = Puppeteer.ConnectAsync(pupLaunchOptions).Result;
+
+            using (var page = browser.NewPageAsync().Result)
+            {
+                page.GoToAsync("https://giaiphapmmo.vn").Wait();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
